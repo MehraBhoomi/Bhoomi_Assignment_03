a0a9362236f2030b30158a3817c379db9f526ab8
MehraBhoomi <137167780+MehraBhoomi@users.noreply.github.com>
2023-07-11 17:46:10 +0530

    Add files via upload

---
+++ Assignment 03 Ques02.py
@@ -0,0 +1,19 @@
+# Write a Python program to reverse a string.
+
+# Sample String : "1234abcd"
+
+# Expected Output : "dcba4321"
+
+
+
+# Solution:
+
+x = input("Enter the text you want to reverse : \n")
+y = "1234abcd"
+
+def reverse(a):
+    print(a[::-1])
+    return ""
+
+reverse(x)
+reverse(y)
